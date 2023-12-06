eb58530eea5933254ff7ade2fab03821fd18686e
Yuhao Gu <yhgu2000@outlook.com>
2023-12-06 21:42:15 +0800

    使能CTest自动化测试

--- cmake/depend.cmake
+++ cmake/depend.cmake
@@ -13,7 +13,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 # 设置调试配置后缀
 set(CMAKE_DEBUG_POSTFIX ".d")
 # 设置可执行和动态库的输出目录
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin/$<CONFIG>")
 
 
 
@@ -26,9 +26,9 @@ add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/utf-8>")
 # 在一个项目里依赖最好不要混用 Boost 动/静态链接
 option(Boost_USE_STATIC_LIBS "是否静态链接 Boost 库。" OFF)
 # set(Boost_DEBUG ON)
-set(_boost_components program_options json coroutine log)
+set(_boost_components program_options log json)
 find_package(Boost REQUIRED COMPONENTS ${_boost_components})
-if (MSVC AND NOT Boost_USE_STATIC_LIBS)
+if(MSVC AND NOT Boost_USE_STATIC_LIBS)
   foreach(_boost_component IN LISTS _boost_components)
     target_link_libraries(Boost::${_boost_component}
       INTERFACE Boost::dynamic_linking)
