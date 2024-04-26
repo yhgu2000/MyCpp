d9ab00fcd4f44617761c16131f6047f4417dd784
Yuhao Gu <yhgu2000@outlook.com>
2024-04-26 12:53:23 +0800

    添加 Boost_NO_WARN_NEW_VERSIONS

--- environ.cmake
+++ environ.cmake
@@ -2,6 +2,7 @@
 set(CODE_COVERAGE ON)
 
 # BOOST 路径
-set(Boost_NO_SYSTEM_PATHS ON)
 set(BOOST_ROOT "C:/Libraries/boost/1_81_0")
+set(Boost_NO_SYSTEM_PATHS ON)
+# set(Boost_NO_WARN_NEW_VERSIONS ON)
 # set(Boost_DEBUG ON)
