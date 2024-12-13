ddd509d1f50e84657e8b43ee935c6b2cdfb4623b
Yuhao Gu <yhgu2000@outlook.com>
2024-12-13 21:04:41 +0800

    发现了续程类的编程范式

--- lib/MyHttp/Client.cpp
+++ lib/MyHttp/Client.cpp
@@ -8,21 +8,79 @@ using namespace My::log;
 
 namespace MyHttp {
 
-struct Client::Connection
+namespace {
+
+using Conn = std::shared_ptr<Client::Connection>;
+
+// 续程类？续程类！
+struct ResolveAndConnect : std::enable_shared_from_this<ResolveAndConnect>
 {
-  std::atomic<Connection*> mNext;
-  std::atomic<Connection*> mPrev;
-  Socket mSocket;
-  ba::steady_timer mTimer;
+  ba::io_context& mIoCtx;
+  std::function<void(Conn)> mCb;
+
+  void exec(My::log::LoggerMt& logger,
+            ba::ip::tcp::resolver& resolver,
+            const std::string& host,
+            const std::string& port)
+  {
+    assert(conn);
+    resolver.async_resolve(
+      host, port, [&, self = shared_from_this()](auto&& a, auto&& b) {
+        on_resolve(logger, a, std::move(b));
+      });
+  }
+
+  void on_resolve(My::log::LoggerMt& logger,
+                  const BoostEC& ec,
+                  ba::ip::tcp::resolver::results_type results)
+  {
+    if (ec) {
+      BOOST_LOG_SEV(logger, warn) << "resolve failed: " << ec.message();
+      return;
+    }
+
+    auto conn = std::make_shared<Client::Connection>(mIoCtx);
+    conn->mSocket.async_connect(
+      *results, [&, conn, self = shared_from_this()](auto&& a) mutable {
+        on_connect(a, std::move(conn));
+      });
+  }
+
+  void on_connect(const BoostEC& ec, Conn conn)
+  {
+    if (ec) {
+      BOOST_LOG_SEV(conn->mLogger, warn) << "connect failed: " << ec.message();
+      return;
+    }
+    mCb(std::move(conn));
+  }
 };
 
+struct RequestAndResponse : std::enable_shared_from_this<RequestAndResponse>
+{
+  std::function<void(BoostResult<Response>&&)> mCb;
+
+  Conn mConn;
+
+  void exec(Conn conn, Request req)
+  {
+    assert(conn);
+    mConn = std::move(conn);
+    // TOOD
+  }
+};
+
+} // namespace
+
 BoostResult<Response>
-Client::http(Request& req) noexcept {
+Client::http(const Request& req) noexcept {
   // TODO
 };
 
 void
-Client::async_http(Request& req,
-                   std::function<void(BoostResult<Response>&&)> cb) {};
+Client::async_http(Request req,
+                   std::function<void(BoostResult<Response>&&)> cb) {
+  // TODO
+};
 
 } // namespace MyHttp
--- lib/MyHttp/Client.hpp
+++ lib/MyHttp/Client.hpp
@@ -2,7 +2,7 @@
 
 #include "util.hpp"
 
-#include <My/Standing.hpp>
+#include <My/Pooled.hpp>
 #include <My/log.hpp>
 #include <atomic>
 
@@ -13,12 +13,30 @@ using namespace util;
 /**
  * @brief 客户端类，实现了 TCP 连接复用。
  */
-class Client : public My::Standing
+class Client
 {
 public:
+  struct Connection : My::Pooled<Connection>
+  {
+    Connection(ba::io_context& ioCtx)
+      : mSocket(ba::make_strand(ioCtx))
+      , mTimer(mSocket.get_executor())
+    {
+    }
+
+    My::log::Logger mLogger{ "Client::Connection", this };
+    Socket mSocket;
+    ba::steady_timer mTimer;
+  };
+
   ba::io_context& mIoCtx;
+
+  // 目标服务器地址
   const std::string mHost;
+  // 目标服务器端口
   const std::string mPort;
+  // 连接超时时间（单位：毫秒）
+  const ba::steady_timer::duration mTimeout{ 3000 };
 
   Client(ba::io_context& ioCtx,
          std::string host,
@@ -39,7 +57,7 @@ public:
    * @param req 请求对象。
    * @return 响应对象。
    */
-  BoostResult<Response> http(Request& req) noexcept;
+  BoostResult<Response> http(const Request& req) noexcept;
 
   /**
    * @brief 异步发送 HTTP 请求。
@@ -47,17 +65,14 @@ public:
    * 这个方法是并发安全的，可以在同时在多个线程中被调用。
    *
    * @param req 请求对象。
-   * @param cb 回调函数，响应结果作为参数。
+   * @param cb 回调续程，响应结果作为参数。
    */
-  void async_http(Request& req,
-                  std::function<void(BoostResult<Response>&&)> cb);
-
-protected:
-  My::log::Logger mLogger;
+  void async_http(Request req, std::function<void(BoostResult<Response>&&)> cb);
 
 private:
-  struct Connection;
-  std::atomic<Connection*> mConn{ nullptr };
+  My::log::LoggerMt mLogger;
+  Connection::Pool mPool;
+  ba::ip::tcp::resolver mResolver{ mIoCtx };
 };
 
 } // namespace MyHttp
